022189ac35d1a4c35796322dec078b6c8490e31b
KaKitLeng <89944580+KaKitLeng@users.noreply.github.com>
2021-09-25 16:56:17 -0400

    Delete Target Support Files directory

--- Target Support Files/Pods-Flix v1-Flix v1UITests/Pods-Flix v1-Flix v1UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flix_v1_Flix_v1UITests : NSObject
-@end
-@implementation PodsDummy_Pods_Flix_v1_Flix_v1UITests
-@end
--- Target Support Files/Pods-Flix v1Tests/Pods-Flix v1Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flix_v1Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Flix_v1Tests
-@end
