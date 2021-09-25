d00205e20da887894f73c39cae622502411d0632
KaKitLeng <89944580+KaKitLeng@users.noreply.github.com>
2021-09-25 18:48:33 -0400

    Unit 2 commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ka Kit Leng on 9/25/21.
+//  Copyright © 2021 Ka Kit Leng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
